6d41f14a14e41a18938d6ac9700aca7bb1558faa
shihui0328 <huangshihui0328@outlook.com>
2020-07-06 17:07:09 +0200

    import iconfont

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="header">
-    <div class="header-left">return</div>
-    <div class="header-input">Where are you going</div>
-    <div class="header-right">City</div>
+
+    <div class="header-left">
+      <span class="iconfont"> &#xe624; </span>
+    </div>
+    <div class="header-input">
+       <span class="iconfont"> &#xe632; </span>
+       Where are you going ?
+    </div>
+    <div class="header-right">City
+      <span class="iconfont" arrow-icon> &#xe64a; </span>
+    </div>
+
   </div>
 </template>
 
@@ -28,15 +37,18 @@ export default {
       flex:1
       height : .64 rem
       line-height : .64 rem
-      margin-top : .12 rem
+      margin-top : .11 rem
       margin-left : .2 rem
       background:#fff
       border-radius: .1 rem
       color : #ccc
+      padding-left : .2 rem
     .header-right
       width: 1.24 rem
       line-height : .86 rem
       float: right
       text-align: center
-
+      .arrow-icon
+        margin-left : .2 rem
+        font-size : .2 rem
 </style>
