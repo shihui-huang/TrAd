fb28dbd93105373b7b52e63c25bd240a19e48b9c
shihui0328 <huangshihui0328@outlook.com>
2020-07-07 22:17:55 +0200

    let swipers scroll in trun

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,9 +2,9 @@
 <div class="wrapper">
  <swiper :options="swiperOption">
     <!-- slides -->
-    <swiper-slide><img class="swiper-img" src="//imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg"/></swiper-slide>
-    <swiper-slide><img class="swiper-img" src="//imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg"/></swiper-slide>
-    <swiper-slide><img class="swiper-img" src="//imgs.qunarzz.com/vs_ceph_vcimg/b8c4527c41649814cc4cf86880abba54.jpeg"/></swiper-slide>
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+        </swiper-slide>
 
   <!-- <swiper-slide>I'm Slide 3</swiper-slide>
     <swiper-slide>I'm Slide 4</swiper-slide>
@@ -23,8 +23,19 @@ export default {
   data () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination'
-      }
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
+      }, {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
+      }, {
+        id: '0003',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vcimg/b8c4527c41649814cc4cf86880abba54.jpeg'
+      }]
     }
   }
 }
