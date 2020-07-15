65e5c94a00eede3c88017d1219dd23e5e5a3423f
shihui0328 <huangshihui0328@outlook.com>
2020-07-15 22:31:04 +0200

    optimize page performance
    Put the loaded page data in the memory through <keep-alive>, when the
    page does not change, there is no need to reload, just take it from
    localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 加载过依稀的内容会放在内存 不会每次刷新都重新加载 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekendRecommend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -29,12 +30,16 @@ export default {
       swiperList: [],
       iconList: [],
       recommendList: [],
-      recommendWeekendList: []
+      recommendWeekendList: [],
+      lastCity: ''
     }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     getHomeInfo () {
-      axios.get('/static/mock/index.json')
+      axios.get('/static/mock/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -50,7 +55,20 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  // 当我们使用keep-alive时会多处一个生命周期函数activated
+  // When we use keep-alive, there will be one more life cycle function: activated
+  // mounted 是第一次加载时会触发 activated是每一次回到home页都时候都会触发
+  // mounted is triggered when it is loaded for the first time. activated is triggered every time it returns to the home page
+  activated () {
+  // 每次加载去判断当前城市是否和上一次都一样 若不一样再发一次ajax请求
+  // Every time you load to determine whether the current city is the same as the last time, if it is not the same, send another ajax request
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
